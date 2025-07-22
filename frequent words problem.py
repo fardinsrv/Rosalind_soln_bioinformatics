5c6910545433c28127a8c616b3bb1e1b9031750c
Fardin Saurov <fardintutul@gmail.com>
2025-07-22 17:21:04 +0600

    Update frequent words problem.py

--- frequent words problem.py
+++ frequent words problem.py
@@ -1,4 +1,14 @@
+# We say that Pattern is a most frequent k-mer in Text if it maximizes Count(Text, Pattern) among all k-mers. For example, "ACTAT" is a most frequent 5-mer in "ACAACTATGCATCACTATCGGGAACTATCCT", and "ATA" is a most frequent 3-mer of "CGATATATCCATAG".
 
+# Frequent Words Problem
+# Find the most frequent k-mers in a string.
+
+# Given: A DNA string Text and an integer k.
+
+# Return: All most frequent k-mers in Text (in any order).
+
+
+#ba1b
 def pattern_count(text,k_mer):
     count = 0
     for i in range(0,len(text)-len(k_mer)+1):
